a18708052a173ace97d60e966dcb286cda26a91c
Austin Dale <ad25016@georgiasouthern.edu>
2024-03-19 16:07:50 -0400

    Added search cities
    You can now check the weather for any city.  There is currently no handling for mispelling/incorrect inputs

--- src/App.js
+++ src/App.js
@@ -5,27 +5,62 @@ import axios from 'axios';
 function App() {
   const [data, setData] = useState("test");
   const [isClicked, setIsClicked] = useState(false);
+  const [search, setSearch] = useState("Statesboro")
+  const [cityName, setCityName] = useState("Statesboro")
+  const [is404, setIs404] = useState(false)
   const url = "https://api.openweathermap.org/data/2.5/weather?q=Statesboro&units=imperial&APPID=5d3ec6e3e5ab7fcc6ad1fb6e70664f61"
 
   const apiKey = "52a8289975620455d5d7cb1ba6bcf616";
 
   function handleClick(e){
-    axios.get(url).then((response) => {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=imperial&APPID=5d3ec6e3e5ab7fcc6ad1fb6e70664f61`).then((response) => {
       setData(response.data)
       console.log(response.data)
+      setIsClicked(true);
+      console.log(search)
+      console.log(data)
+      setCityName(search)
     })
-    setIsClicked(true);
   }
 
   if(!isClicked) {
     handleClick();
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(search === ""){
+        return
+    }
+    handleClick()
+  }
+
+  const handleChange = (value) => {
+      let tempSearch = search
+      tempSearch = value
+      setSearch(tempSearch)
+  }
+
   return (
     <div className="App">
-      {isClicked?(
+      {!is404? 
+        <div>
+           {isClicked?(
         <div className='maina'>
           <header>
+            <div className='searchBox'>
+              <form onSubmit={handleSubmit} className='loginForm'>
+                <label className='passwordBox'>
+                  <input 
+                    type="text" 
+                    name="city" 
+                    placeholder="search city here"
+                    onChange={(e) => {handleChange(e.target.value)}}
+                  />
+                 </label>
+                <input type="submit" />
+              </form>
+            </div>
             <p>Weather by Austin Dale</p>
           </header>
           <aside className='left'>
@@ -35,11 +70,11 @@ function App() {
             <div className="">
               {data.main?(
                 <>
-                  <h1>Current temp {data.main.temp}</h1>
-                  {data.weather?(<p>Current weather {data.weather[0].main}</p>) : <p>weather messed up</p>}             
-                  <p>Current speed {data.wind.speed}</p>
-                  <p>Feels like {data.main.feels_like}</p>
-                  <p>Current humidity {data.main.humidity}</p>
+                  <h1>{cityName} Current temp {data.main.temp}</h1>
+                  {data.weather?(<h2>Current weather {data.weather[0].main}</h2>) : <p>weather messed up</p>}             
+                  <h2>Current speed {data.wind.speed}</h2>
+                  <h2>Feels like {data.main.feels_like}</h2>
+                  <h2>Current humidity {data.main.humidity}</h2>
                 </>
               ) : (
                 <></>
@@ -50,12 +85,15 @@ function App() {
             <p>10 Day</p>
           </aside>  
           <footer>
-          <p>https://github.com/AustinSoar</p>
+          <a href="https://github.com/AustinSoar">GitHub</a>
           </footer>
        </div>
       ) : (
             <></>
           )}
+        </div>
+      : <div>Please check spelling of city</div>}
+     
     </div>
   );
 }
